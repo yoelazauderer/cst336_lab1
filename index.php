f8fcfc944c51cf2735ec830f18a603f2b7a78d5a
EC2 Default User <ec2-user@ip-172-31-8-36.us-west-1.compute.internal>
2020-10-27 05:37:14 +0000

    adding home page

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 1: Personal Page </title>
+        <meta charset="utf-8">
     </head>
     <body>
         
-        <h1>Welcome to my website!</h1>
+        <header>
+            <h1> Yoela Zauderer </h1>
+        </header>
+        
+        <hr>
+        
+        <nav>
+            
+            <a href="index.html"> Home </a>
+            <a href="index.html"> About </a>
+            <a href="index.html"> Contact </a>
+            
+        </nav>
+        
+        <main>
+            
+            <figure>
+                <img src="img/juan_doe.png">
+            </figure>
+            
+            <div>
+                Welcome to my site!
+            </div>
+            
+            
+        </main>
+        
+        <footer>
+            
+        </footer>
+        
 
     </body>
 </html>
